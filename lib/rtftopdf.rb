ea041433507c33e680f47b9b124fb2ce2e6c833d
Darren Cauthon <darren@cauthon.com>
2013-12-17 12:59:20 -0600

    That should be a module.

--- lib/rtftopdf.rb
+++ lib/rtftopdf.rb
@@ -19,7 +19,7 @@ class RTF
   end
 end
 
-class RTFtoPDF
+module RTFtoPDF
   def self.to_pdf rtf_content
     html_content = RTF.new(rtf_content).to_html
     pdf_content = PDFKit.new(html_content).to_pdf
