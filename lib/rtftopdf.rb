b1b3d1c67bf744abf30772ac3456d579d110b030
Caleb Cauthon <ccauthon@rednovalabs.com>
2013-12-17 09:51:20 -0600

    Added RTF to HTML conversion.

--- lib/rtftopdf.rb
+++ lib/rtftopdf.rb
@@ -3,11 +3,18 @@ require "pdfkit"
 
 class RTF
 	def initialize rtf_content
-
+    @rtf_content = rtf_content
   end
 
   def to_html
+    temporary_rtf_file = Tempfile.new('RTF')
+    temporary_rtf_file.write(@rtf_content)
+    temporary_rtf_file.close
 
+    html_content = `unrtf #{temporary_rtf_file.path}`
+    temporary_rtf_file.unlink
+
+    html_content
   end
 end
 
--- test/lib/rtftopdf_test.rb
+++ test/lib/rtftopdf_test.rb
@@ -1,9 +1,26 @@
 require 'test_helper'
+require 'tempfile'
 
 describe "rtf to pdf conversion" do
-	it "must turn rtf input into html" do
+	describe "rtf to html conversion" do
+    it "must turn rtf input into html" do
+      rtf_input = '{\rtf1\ansi\ansicpg1252\deff0\deflang1033\deflangfe1033{\fonttbl{\f0\froman\fprq2\fcharset0 Times New Roman;}{\f1\fnil\fcharset0 Times New Roman;}{\f2\fnil Times New Roman;}{\f3\fswiss\fprq2\fcharset0 Arial;}}\viewkind4\uc1\pard\nowidctlpar\qr\b\f0\fs18 LOREM ISPO FACTO\par\pard\nowidctlpar\qc\f1\fs28 LOREM IPSEUM AGREEMENT\f0\fs18                                                                         \par}'
+      temporary_rtf_file = Tempfile.new('RTF')
+      temporary_rtf_file.write(rtf_input)
+      temporary_rtf_file.close
 
-	end
+      html_content = `unrtf #{temporary_rtf_file.path}`
+      temporary_rtf_file.unlink
+
+      PDFKit.expects(:new).with(html_content)
+
+      RTFtoPDF.to_pdf(rtf_input)
+  	end
+    it "must delete the temporary file when its finished" do
+      Tempfile.any_instance.expects(:unlink)
+      RTFtoPDF.to_pdf("asdf")
+    end
+  end
 	it "must pass on the html result to pdf kit" do
 		RTF.any_instance.stubs(:to_html).returns("my html content!")
 
